e339069cd10a8a660756fefeadb5db1850ae824f
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-11-02 10:04:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,6 +16,10 @@ class CashRegister
 
   def add_item(title, price, quantity=1)
     self.total = price * quantity
+    quantity.times do
+      items << total
+    end
+    self.last_transaction = price * quantity
   end
 
 end
