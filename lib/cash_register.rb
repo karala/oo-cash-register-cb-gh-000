ec00c8d79a5aa1e9cf6db99e642cbaf2356694ed
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-11-02 09:46:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
   attr_accessor :employee_discount
 
-  def initialize(employee_discount)
+  def initialize(employee_discount = 0)
     @total = 0
     @employee_discount = employee_discount
   end
